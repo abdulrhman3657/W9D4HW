2f094e39f95f3f3eb106a83b7df29cb0b08f3436
user1 <user01@em/com>
2025-06-18 21:12:03 +0300

    final v1

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,10 +1,10 @@
-import Dealer from "../models/car.js"
+import Car from "../models/car.js"
 
 export const createDealer = async (req, res) => {
 
     try {
 
-        const dealer = new Dealer(req.body)
+        const dealer = new Car(req.body)
         await dealer.save();
 
         res.status(201).json(dealer)
@@ -21,7 +21,7 @@ export const createDealer = async (req, res) => {
 export const getDealers = async (req, res) => {
     try {
 
-        const dealers = await Dealer.find()
+        const dealers = await Car.find()
         res.status(200).json(dealers)
         
     } catch (error) {
@@ -37,7 +37,7 @@ export const getDealer = async (req, res) => {
     try {
 
         const { id } = req.params
-        const dealer = await Dealer.findOne({ _id: id })
+        const dealer = await Car.findOne({ _id: id })
         res.status(200).send(dealer)
         
     } catch (error) {
@@ -56,7 +56,7 @@ export const updateDealer = async (req, res) => {
 
         const dealer = req.body;
 
-        const updatedDealer = await Dealer.findByIdAndUpdate(id, dealer, { new: true });
+        const updatedDealer = await Car.findByIdAndUpdate(id, dealer, { new: true });
 
 
         res.status(200).send(updatedDealer)
@@ -75,9 +75,49 @@ export const deleteDealer = async (req, res) => {
 
         const { id } = req.params
 
-        await Dealer.findByIdAndDelete(id)
+        await Car.findByIdAndDelete(id)
 
         res.status(200).json({message: "dealer deleted successfully"})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            error: error
+        })
+    }
+}
+
+// Get_all_cars_by_dealerId
+export const Get_all_cars_by_dealerId = async (req, res) => {
+    try {
+
+        // const { id } = req.params.dealerID
+        const id = req.params.dealerID
+
+
+        console.log(id)
+
+        const dealer = await Car.findOne({ dealerId: id })
+
+        console.log(dealer)
+
+        res.status(200).send(dealer)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            error: error
+        })
+    }
+}
+
+// Get_all_cars_by_carMakeId
+export const Get_all_cars_by_carMakeId = async (req, res) => {
+    try {
+
+        const id = req.params.carMakeId
+        const dealer = await Car.findOne({ dealerId: id })
+        res.status(200).send(dealer)
+
         
     } catch (error) {
         console.log(error)
--- routes/carRoute.js
+++ routes/carRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createDealer, deleteDealer, getDealer, getDealers, updateDealer } from "../controllers/car.controller.js";
+import { createDealer, deleteDealer, Get_all_cars_by_carMakeId, Get_all_cars_by_dealerId, getDealer, getDealers, updateDealer } from "../controllers/car.controller.js";
 
 const router = express.Router();
 
 router.get("/", getDealers)
+router.get("/dealer/:dealerID", Get_all_cars_by_dealerId)
+router.get("/carmake/:carMakeId", Get_all_cars_by_carMakeId)
+
 router.post("/", createDealer)
 router.get("/:id", getDealer)
 router.put("/:id", updateDealer)
